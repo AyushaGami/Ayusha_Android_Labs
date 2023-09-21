333c9dfd388a1981a91005473921c0e26900c366
AyushaGami <patoriyaa@yahoo.in>
2023-09-21 12:00:47 -0400

    commit

--- app/src/main/java/algonquin/cst2335/ayushaandroidlabs/ui/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/ayushaandroidlabs/ui/MainActivity.java
@@ -1,52 +1,61 @@
 package algonquin.cst2335.ayushaandroidlabs.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import algonquin.cst2335.ayushaandroidlabs.R;
+import android.widget.Toast;
 import algonquin.cst2335.ayushaandroidlabs.data.MainViewModel;
 import algonquin.cst2335.ayushaandroidlabs.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity
 {
-    private ActivityMainBinding binding;
-    private MainViewModel viewModel;
+    private ActivityMainBinding variableBinding;
+    private MainViewModel Model;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        Model = new ViewModelProvider(this).get(MainViewModel.class);
+        variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
         //this is the only function call, loads stuff onto screen
-        setContentView(binding.getRoot());
+        setContentView(variableBinding.getRoot());
 
-        TextView tv = binding.myTextView;
-        Button b = binding.myButton;
-        EditText et = binding.myEditText;
-
-        viewModel.userString.observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(String s) {
-                tv.setText(s);
-            }
-        });
-        et.setText(viewModel.userString.getValue());
-
-        b.setOnClickListener(v ->
+        variableBinding.myButton.setOnClickListener(click ->
         {
-            tv.setText(R.string.button_message);
-            String string = et.getText().toString();
-            viewModel.userString.postValue (string);
-            b.setText("you clicked the button");
-
+            Model.editString.postValue(variableBinding.myEditText.getText().toString());
         });
+        Model.editString.observe(this, s ->
+        {
+            variableBinding.myTextView.setText("Your edit text has " + s);
+        });
+        Model.isSelected.observe(this, selected ->
+        {
+            variableBinding.myCheckbox.setChecked((Boolean) selected);
+            variableBinding.mySwitch.setChecked((Boolean) selected);
+            variableBinding.myRadioButton.setChecked((Boolean) selected);
+        });
+        variableBinding.myCheckbox.setOnCheckedChangeListener((btn, isChecked) ->
+        {
+            Model.isSelected.postValue(isChecked);
+            showToast("CheckBox clicked");
+        });
+        variableBinding.myCheckbox.setOnCheckedChangeListener((btn, isChecked) ->
+        {
+            Model.isSelected.postValue(isChecked);
+            showToast("Switch clicked");
+        });
+        variableBinding.myCheckbox.setOnCheckedChangeListener((btn, isChecked) ->
+        {
+            Model.isSelected.postValue(isChecked);
+            showToast("RadioButton clicked");
+        });
+        variableBinding.myImageButton.setOnClickListener(i ->
+        {
+            showToast("The width = " + i.getWidth() + "And the height = " + i.getHeight());
+        });
+    }
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 }
