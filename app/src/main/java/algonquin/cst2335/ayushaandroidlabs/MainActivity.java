1b67799e9fadeb6ba2a2f4043bac1cd333d266be
AyushaGami <patoriyaa@yahoo.in>
2023-09-29 14:25:59 -0400

    I am Finished w2ith phone version lab 3 Spin

--- app/src/main/java/algonquin/cst2335/ayushaandroidlabs/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/ayushaandroidlabs/MainActivity.java
@@ -1,16 +1,39 @@
 package algonquin.cst2335.ayushaandroidlabs;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imgView;
+    Switch sw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //this is the only function call, loads stuff onto screen
         setContentView(R.layout.activity_main);
+
+        imgView = findViewById(R.id.flagview);
+        sw = findViewById(R.id.switch1);
+
+        sw.setOnCheckedChangeListener( (btn, isChecked) -> {
+            if (isChecked)
+            {
+                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF,
+                        0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                rotate.setDuration(5000);
+                rotate.setRepeatCount(Animation.INFINITE);
+                rotate.setInterpolator(new LinearInterpolator());
+
+                imgView.startAnimation(rotate);
+            }
+            else {
+                imgView.clearAnimation();
+            }
+        });
     }
 }
